ef0104617cb74748c37b57a98b2e65d6d250f084
Loreen <loreenmuusha.com>
2020-12-09 15:23:38 +0200

    getUserId and getUserLevel functions added

--- zoo_functions.js
+++ zoo_functions.js
@@ -15,7 +15,17 @@ module.exports = function zooFact(pool) {
         return insertUserQuery
     }
 
-    function getVideoUrl(levelId) {
+    function getUserId(name) {
+        let getUserIdQuery = (`SELECT id FROM users WHERE user_name = ($1)`, [name])
+        return getUserIdQuery.rows
+    }
+
+    function getUserLevel(name) {
+        let userId = getUserId(name)
+        let getUserLevel = (`SELECT level_act_id FROM progress WHERE user_id = $1`, [userId])
+    }
+
+    function getVideoUrl(level_id) {
         let getVideoUrlQuery = (`SELECT video_url 
     FROM level_acts WHERE motion = $1`, [level_id])
         return getVideoUrlQuery.rows;
@@ -26,6 +36,8 @@ module.exports = function zooFact(pool) {
     return {
         greetUser,
         storeUser,
-        getVideoUrl
+        getUserId,
+        getVideoUrl,
+        getUserLevel
     }
 }
