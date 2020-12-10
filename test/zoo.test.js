e45f2271171d006b985392fec1473790a8688453
Sibonisiwe Sodinga <sodinga91@gmail.com>
2020-12-10 09:23:24 +0200

    added tests

--- test/zoo.test.js
+++ test/zoo.test.js
@@ -9,11 +9,11 @@ const connectionString = process.env.DATABASE_URL || 'postgresql://codex:codex12
 const pool = new Pool({
     connectionString
 });
-// describe("The zoo App ", function(){
-// beforeEach(async function () {
-//     await pool.query("delete from ");
+describe("The zoo App ", function(){
+beforeEach(async function () {
+    await pool.query("delete from users");
 
-// });
+});
 
 describe('The greetUser function', function(){
     it('should be able to greet Chuma', async function(){
@@ -24,31 +24,45 @@ describe('The greetUser function', function(){
 });
 
 describe('The storeUser function', function(){
-    it('should be able to store the name Yeu into table', async function(){
+    it('should be able to store the name Bantu into table', async function(){
         let zoo = zooFact(pool)
 
-        await zoo.storeUser('Bantu')
-        assert.equal('Bantu', await zoo.storeUser('Bantu')); 
+        let user = await zoo.storeUser('Bantu') 
+        assert.equal(user, await zoo.storeUser('Bantu')); 
     });
 
     it('should be able to insert the name Yeu in the table', async function(){
         let zoo = zooFact(pool)
-        await zoo.storeUser('Yeu')
-        assert.equal('Yeu', await zoo.storeUser('Yeu')); 
+        
+        let user = await zoo.storeUser('Yeu') 
+        assert.equal(user, await zoo.storeUser('Yeu')); 
     });
 });
 
 describe('The getUserId function', function(){
     it('should be able to return Loreen ID', async function(){
         let zoo = zooFact(pool)
+        
         await zoo.storeUser('Loreen')
-        assert.equal(1, await zoo.getUserId('Loreen'))
+        let id =  await zoo.getUserId('Loreen')
+        let userId = await pool.query(`SELECT id FROM users WHERE user_name = 'Loreen'`)
+        assert.deepEqual(userId.rows, id)
+    })
+});
+
+describe('The getUserLevel function', function(){
+    it('should be able to return user level', async function(){
+    let zoo = zooFact(pool)
+
+    //let user = await zoo.storeUser('Sibo') 
+    let userId = await pool.query(`SELECT id FROM users WHERE user_name = 'Yeu'`)
+    assert.deepEqual(userId, await zoo.getUserLevel())
     })
 })
 
-// after(function() {
-//     pool.end();
-// });
+after(function() {
+    pool.end();
+});
 
 
-// });
+});
